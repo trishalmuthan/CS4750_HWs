c3f67cd2148645686fa4ecc5094ecf3ad1e71b01
trishalmuthan <trishalmuthan@gmail.com>
2025-01-25 16:04:32 -0500

    Added reading in json and connecting to sqlite

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -3,8 +3,29 @@ import sqlite3
 import sys
 
 def main(json_file, sqlite_file):
-    print(json_file)
-    print(sqlite_file)
+    print('Reading json file to database.')
+
+    with open(json_file) as f:
+        data = json.load(f)
+    
+    try:
+        books = data['books']
+    except KeyError:
+        print('There is no books key in the given json file.')
+        print('Database was not created.')
+        return
+
+    sqlite_connection = sqlite3.connect(sqlite_file)
+    sqlite_cursor = sqlite_connection.cursor()
+
+    #CREATE TABLE QUERY
+
+
+    #INSERT VALUES QUERY
+
+
+    sqlite_connection.close()
+    print('Completed reading json file to database.')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
