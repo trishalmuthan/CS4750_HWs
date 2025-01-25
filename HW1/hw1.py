011f29f33c58f7006cf16bfdaada83c2674e2069
trishalmuthan <trishalmuthan@gmail.com>
2025-01-25 15:55:07 -0500

    Added input

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,1 +1,13 @@
-print("HW 1")
+import json
+import sqlite3
+import sys
+
+def main(json_file, sqlite_file):
+    print(json_file)
+    print(sqlite_file)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Correct Usage: python hw1.py <JSON file> <.sqlite DB file>')
+    else:
+        main(sys.argv[1], sys.argv[2])
